a576a9ce634f03488a1bcc29ca94694cc4916e5e
Florent T <94051212+flotql@users.noreply.github.com>
2022-01-31 19:43:42 +0100

    Update Bibliotheque.py

--- Classes/Bibliotheque.py
+++ Classes/Bibliotheque.py
@@ -38,7 +38,7 @@ class Biblio:
             print("id ",i[0],i[1],i[2])
 
     def __repr__(self):
-        choix = input("1: Liste Utilisateurs \n2: Liste Livres")
+        choix = input("1: Liste Utilisateurs \n2: Liste Livres\n")
         affiche = ""
         if choix == "1":
             for i in self.utilisateurs:
@@ -46,17 +46,47 @@ class Biblio:
         elif choix == "2":
             for i in self.livres:
                 affiche += i+"\n"
-        else: print("Erreur, 1 ou 2 seulement")
+        else:
+            print("Erreur, 1 ou 2 seulement")
         return affiche
 
+    def rechercheLivre(self):
+        choix = input("1: Tous \n2: Disponnibles")
+        if choix == "1":
+            cat = input("Recherche par: \n\t1: Catégorie \n\t2: Auteur \t\n3: Genre \t\n4: Langue")
+            if cat == "1":
+
+            if cat == "2":
+
+            if cat == "3":
+
+            if cat == "4":
+
+            else:
+                print("Erreur, de 1 à 4 seulement")
+        elif choix == "2":
+            cat = input("Recherche par: \n\t1: Catégorie \n\t2: Auteur \t\n3: Genre \t\n4: Langue")
+            if cat == "1":
+
+            if cat == "2":
+
+            if cat == "3":
+
+            if cat == "4":
+
+            else:
+                print("Erreur, de 1 à 4 seulement")
+        else:
+            print("Erreur, 1 ou 2 seulement")
 
 
 test = Biblio()
 test.importUtilisateurs()
 test.afficherUtilisateurs()
+print (test)
 
     # repr -faire choix si 1 afficher utilisateurs 2 afficher livres ? DONE
-    # faire une recherche liste rayon, livres utilisateurs ?
+    # faire une recherche liste rayon, livres utilisateurs ? EN COURS
     # faire l'ajout d'utilisateurs ou livres ici?
     # les emprunts ici?
     # faire un emprunt /prolonger emprunt
