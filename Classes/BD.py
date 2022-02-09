0c18a48d90e4d2e554093d7d1d63897393317afe
gaetancorin <gaetancorin@gmail.com>
2022-02-09 17:00:27 +0100

    repr de livre et bd

--- Classes/BD.py
+++ Classes/BD.py
@@ -8,6 +8,14 @@ class BD(Livre):
         self.couleur = couleur
         self.dessinateur = dessinateur
 
+    def __repr__(self):
+        return str("Voici les éléments de la BD:\n\t"
+                   "Titre: "+self.titre+"      Auteur: "+self.auteur+"\n\t"
+                   "Langue: "+self.langue+"      Genre: "+self.genre+"\n\t"
+                   "Categorie: "+self.categorie + "      Référence: "+self.ref+"\n\t"
+                   "Disponibilité: "+self.dispo + "      Date de retour: "+self.retour+"\n\t"
+                    "En couleur: "+self.couleur + "      Dessinateur: "+self.dessinateur+"\n\t")
+
 
 # b = BD("Injustice 1", "DC", "Français", "Fantastique", "BD", "ID104382", "True", "None", "True", "J Raapack")
-# print(b.categorie, b.dessinateur)
+# print(b)
