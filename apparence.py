2259028f8edaa8137db126cf6990191f35de61c9
YapYap31 <yapyap880@gmail.com>
2022-02-22 12:20:58 +0100

    pas vraiment utile ici

--- apparence.py
+++ apparence.py
@@ -1,14 +1,3 @@
-import os
-# récupérer le chemin du répertoire courant
-path = os.getcwd()
-print("Le répertoire courant est : " + path)
-# récupérer le nom du répertoire courant
-repn = os.path.basename(path)
-print("Le nom du répertoire est : " + repn)
-# récupérer le chemin du script
-path = os.path.realpath(__file__)
-print("Le chemin du script est : " + path)
-
 ## le code ci-dessous à mettre dans la console :
 # python -m ensurepip
 # python -m pip install art
