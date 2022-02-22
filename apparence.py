713390e7060bb7b7814ec3204a77cdfdb66c0253
YapYap31 <yapyap880@gmail.com>
2022-02-22 12:07:43 +0100

    ajout d'un fichier apparence.py

---
+++ apparence.py
@@ -0,0 +1,22 @@
+import os
+# récupérer le chemin du répertoire courant
+path = os.getcwd()
+print("Le répertoire courant est : " + path)
+# récupérer le nom du répertoire courant
+repn = os.path.basename(path)
+print("Le nom du répertoire est : " + repn)
+# récupérer le chemin du script
+path = os.path.realpath(__file__)
+print("Le chemin du script est : " + path)
+
+## le code ci-dessous à mettre dans la console :
+# python -m ensurepip
+# python -m pip install art
+
+from art import *
+# art.aprint("random")
+tprint("Bonjour",font="block-medium")
+tprint("Bienvenue dans la Bibliotheque",font="block-medium")
+
+# Liste des polices :
+# https://github.com/sepandhaghighi/art/blob/master/FontList.ipynb
